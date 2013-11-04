48947af9c3cbec3023a167b1e424f4b1c2054541
Lero <lero@lerolero>
2013-11-04 19:06:14 -0300

    Comentarios quedaron para la proxima clase

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,48 @@
 class PostsController < ApplicationController
 
 def new
+@post = Post.new()
+
+end
+
+def show
+  @post = Post.find(params[:id])
 end
 
 def index
   @posts = Post.all
 end
 
+def edit
+  @post = Post.find(params[:id])
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+ 
+  redirect_to posts_path
+end
+
+
+def update
+  @post = Post.find(params[:id])
+ 
+  if @post.update(params[:post].permit(:title, :text))
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
+
 def create
   @post = Post.new(post_params)
  
-  @post.save
-  redirect_to @post
+  if  @post.save
+  	redirect_to @post
+  else
+	render 'new'
+  end
 end
  
 private
@@ -20,3 +51,4 @@ private
   end
 
 end
+
